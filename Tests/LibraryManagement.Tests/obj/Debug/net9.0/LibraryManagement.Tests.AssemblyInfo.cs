0a8a0b0e424738c9952eea655fd539a4ef8eba84
Aref Zangeneh <aref.za.94@gmail.com>
2025-07-29 20:55:59 +0330

    Projects' dependencies on other projects applied based on Clean Architecture principles. Application -> Domain Infrastrucure -> Application & Domain API -> Infrastructure & Application Test -> Domain, Application & Infrastructure

--- Tests/LibraryManagement.Tests/obj/Debug/net9.0/LibraryManagement.Tests.AssemblyInfo.cs
+++ Tests/LibraryManagement.Tests/obj/Debug/net9.0/LibraryManagement.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8c8c93d83498ca06ab0071845d1baa960db38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679a8f27af943acd7cbc889b29f83beadcaa9a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
