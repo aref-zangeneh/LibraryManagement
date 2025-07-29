679a8f27af943acd7cbc889b29f83beadcaa9a76
Aref Zangeneh <aref.za.94@gmail.com>
2025-07-29 20:51:37 +0330

    Test project (xUnit) added.

--- src/Infrastructure/LibraryManagement.Infrastructure/obj/Debug/net9.0/LibraryManagement.Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/LibraryManagement.Infrastructure/obj/Debug/net9.0/LibraryManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7584562c6296a8057a36bafa107d4646ed0a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8c8c93d83498ca06ab0071845d1baa960db38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
